180cf80ff140491032b0b5855496869ee868d841
João Gabriel <main.joaogabrielsg@gmail.com>
2021-11-05 10:06:12 -0300

    feat: add module 23 project

---
+++ 23-module-project/pages/index.js
@@ -0,0 +1,45 @@
+import { MongoClient } from "mongodb";
+import Head from "next/head";
+
+import MeetupList from "../components/meetups/MeetupList";
+
+function HomePage(props) {
+  return (
+    <>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    `mongodb+srv://jgsg:DxPMlUdqMMqIWUSp@cluster0.alpzw.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        id: meetup._id.toString(),
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+      })),
+    },
+    revalidate: 10,
+  };
+}
+
+export default HomePage;
